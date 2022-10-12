fa0436b575f75a6a4f780952c4cebd4c7276ae95
GromDV <gromdv@gmail.com>
2022-10-12 10:33:17 +0300

    add Lesson-5

---
+++ Lesson-5_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ваше имя: ");
+string userName = Console.ReadLine();
+
+if(userName.ToLower() == "маша")
+{
+    Console.WriteLine("Это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
